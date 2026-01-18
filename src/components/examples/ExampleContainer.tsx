d90c69a1283d925e75e08e233ddc7b3cb507a9e4
CMRicardo <rcorrales@grupocomidas.com>
2026-01-18 16:19:22 -0600

    Update ExampleContainer styles for improved layout and spacing

--- src/components/examples/ExampleContainer.tsx
+++ src/components/examples/ExampleContainer.tsx
@@ -1,3 +1,3 @@
 export const ExampleContainer = ({ children }: { children: React.ReactNode }) => {
-  return <div className="grid place-items-center h-32 w-full">{children}</div>
+  return <div className="grid place-items-center px-4 py-8 w-full rounded-md">{children}</div>
 }
