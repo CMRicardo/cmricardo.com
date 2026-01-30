a477e257098b8c9ad6152f857261e2cff9a3eb9e
CMRicardo <rcorrales@grupocomidas.com>
2026-01-30 15:59:12 -0600

    Use absolute imports and format DrawerDemo

--- src/components/examples/DrawerDemo.tsx
+++ src/components/examples/DrawerDemo.tsx
@@ -1,31 +1,39 @@
-import { AlertIcon, CancelIcon } from "@hugeicons/core-free-icons"
-import { Icon } from "../Icon"
-import { Button } from "../ui/button"
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "../ui/drawer"
-import { ExampleContainer } from "./ExampleContainer"
+import { AlertIcon, CancelIcon } from "@hugeicons/core-free-icons";
+import { Icon } from "@/components/Icon";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { ExampleContainer } from "@/components/examples/ExampleContainer";
 
 export const DrawerDemo = () => {
   return (
     <ExampleContainer>
       <Drawer>
-        <DrawerTrigger asChild >
-          <Button variant="destructive" >
+        <DrawerTrigger asChild>
+          <Button variant="destructive">
             <Icon icon={AlertIcon} />
             Delete profile
           </Button>
         </DrawerTrigger>
-        <DrawerContent className="max-w-sm mx-auto" >
+        <DrawerContent className="max-w-sm mx-auto">
           <DrawerHeader>
-            <DrawerTitle>
-              Delete profile
-            </DrawerTitle>
+            <DrawerTitle>Delete profile</DrawerTitle>
             <DrawerDescription>
-              Are you sure you want to delete your profile? This action cannot be undone.
+              Are you sure you want to delete your profile? This action cannot
+              be undone.
             </DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
-            <DrawerClose asChild >
-              <Button variant={'secondary'} >
+            <DrawerClose asChild>
+              <Button variant={"secondary"}>
                 <Icon icon={CancelIcon} />
                 Cancel
               </Button>
@@ -38,5 +46,5 @@ export const DrawerDemo = () => {
         </DrawerContent>
       </Drawer>
     </ExampleContainer>
-  )
-}
+  );
+};
